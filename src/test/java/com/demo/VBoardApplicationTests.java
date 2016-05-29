c2285b963482b3b6095e2f1a10c21ecb1fd58b81
Ruben Nieva <ruben.nieva@gmail.com>
2016-05-28 21:34:17 -0300

    first commit

---
+++ src/test/java/com/demo/VBoardApplicationTests.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = VBoardApplication.class)
+@WebAppConfiguration
+public class VBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
